844cb3d5d1f818676897d3e4fddf9f2899845b0b
Antoine Perry <antoine.ap.57@gmail.com>
2020-03-26 19:48:39 +0100

    graph gen

--- lab3/MSI_MESI.py
+++ lab3/MSI_MESI.py
@@ -35,26 +35,63 @@ MESI_65536 = mean_cpu_cores([CPU_1_mesi_65536, CPU_2_mesi_65536, CPU_3_mesi_6553
 MSI = [MSI_1024, MSI_2048, MSI_32768, MSI_65536]
 MESI = [MESI_1024, MESI_2048, MESI_32768, MESI_65536]
 
+data_array = {0: "CPUId",
+              1: "numReadHits",
+              2: "numReadMisses",
+              3: "numReadOnInvalidMisses",
+              4: "numReadRequestsSent",
+              5: "numReadMissesServicedByOthers",
+              6: "numReadMissesServicedByShared",
+              7: "numReadMissesServicedByModified",
+              8: "numWriteHits",
+              9: "numWriteMisses",
+              10: "numWriteOnSharedMisses",
+              11: "numWriteOnInvalidMisses",
+              12: "numInvalidatesSent",
+              13: "numInclusionMisses",
+              14: "numInclusionHits",
+              15: "numLLCMisses",
+              16: "numLLCHits"
+              }
+
 plt.figure()
 plt.xlabel("Array Size")
-plt.ylabel("Read Hits")
-plt.plot(x, filter_array(MSI, 1), label="MSI")
-plt.plot(x, filter_array(MESI, 1), label="MESI")
+plt.ylabel("Read Requests")
+plt.plot(x, filter_array(MSI, 1), label="MSI Hits")
+plt.plot(x, filter_array(MESI, 1), label="MESI Hits")
+plt.plot(x, filter_array(MSI, 2), label="MSI Misses")
+plt.plot(x, filter_array(MESI, 2), label="MESI Misses")
+plt.legend(loc="upper right")
+plt.show()
+
+
+
+
+plt.figure()
+plt.xlabel("Array Size")
+plt.ylabel("Miss Serviced by")
+plt.plot(x, filter_array(MSI, 6), label="Shared in MSI")
+plt.plot(x, filter_array(MSI, 7), label="Modified in MSI")
+plt.plot(x, filter_array(MESI, 6), label="Shared in MESI")
+plt.plot(x, filter_array(MESI, 7), label="Shared in MESI")
+plt.legend(loc="upper right")
 plt.show()
 
 plt.figure()
 plt.xlabel("Array Size")
-plt.ylabel("Read Misses")
-plt.plot(x, filter_array(MSI, 2), label="MSI")
-plt.plot(x, filter_array(MESI, 2), label="MESI")
+plt.ylabel("Write Requests")
+plt.plot(x, filter_array(MSI, 8), label="MSI Hits")
+plt.plot(x, filter_array(MSI, 9), label="MSI Misses")
+plt.plot(x, filter_array(MESI, 8), label="MESI Hits")
+plt.plot(x, filter_array(MESI, 9), label="MESI Misses")
+plt.legend(loc="upper right")
 plt.show()
-
 
 plt.figure()
 plt.xlabel("Array Size")
 plt.ylabel("Read Requests Sent")
-plt.plot(x, filter_array(MSI, 4), label="MSI")
-plt.plot(x, filter_array(MESI, 4), label="MESI")
+plt.plot(x, filter_array(MSI, 12), label="MSI")
+plt.plot(x, filter_array(MSI, 12), label="MSI")
+plt.legend(loc="upper right")
 plt.show()
 
-# BENCHMARK 2
